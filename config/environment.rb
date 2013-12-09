49528307d1c882b48afe464789a823d499d7c7fc
Kolomoets Oleg <okolomoets@prophonix.com>
2013-12-09 17:35:01 +0200

    rails_apps_composer: initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+UnicornApache::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnicornApache::Application.config.secret_key_base = 'd6296ec16fce97d75d06680aba7608bc784bcb6d8a5e2e4401cb8f2b9d4e0c0ddbd827650459d28afc8b3e76e503d591f7358f04110190a44a0cb34e121c504d'
