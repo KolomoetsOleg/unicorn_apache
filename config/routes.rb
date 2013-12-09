287432bec304b038f13dcc4930ee39c516ee06cf
Kolomoets Oleg <okolomoets@prophonix.com>
2013-12-09 17:36:13 +0200

    rails_apps_composer: models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 UnicornApache::Application.routes.draw do
+  apipie
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
